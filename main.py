4da7f82b87e7c5ba71858f53ade1ad79a3742b06
KeanuOrig <keanujohn@yahoo.com>
2025-05-01 18:42:11 +0800

    finalize

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ app = FastAPI()
 # Allow CORS for local dev and deployed frontend
 origins = [
     "http://localhost:5173",
-    "https://test-1rxpe0vvn-keanus-projects-916bf33d.vercel.app/",
+    "https://test-1rxpe0vvn-keanus-projects-916bf33d.vercel.app",
     "https://test-6xk0wqjbd-keanus-projects-916bf33d.vercel.app",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # All for now
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
