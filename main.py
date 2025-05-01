712721ed7e83c80b31c8b69688795036734cb654
KeanuOrig <keanujohn@yahoo.com>
2025-05-01 18:23:57 +0800

    finalize

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # All for now
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
